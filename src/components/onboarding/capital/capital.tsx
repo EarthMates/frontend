44f41a3bda61d9d843f95542c1815b6ded8a7a99
JacopoCeravolo <77071770+JacopoCeravolo@users.noreply.github.com>
2024-05-18 14:21:08 +0200

    embedded slider in capital component

--- src/components/onboarding/capital/capital.tsx
+++ src/components/onboarding/capital/capital.tsx
@@ -2,11 +2,13 @@ import classNames from "classnames";
 import styles from "./capital.module.scss";
 
 import { Button } from "../button/button";
+import { useState } from "react";
+import InputSlider from "../inputSlider/input-slider";
 
 export interface CapitalProps {
   className?: string;
   capitalAmount: number;
-  handleCapitalChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleCapitalChange: (value: number) => void;
   handleForward: () => void;
 }
 
@@ -16,13 +18,19 @@ export const Capital = ({
   handleCapitalChange,
   handleForward,
 }: CapitalProps) => {
+  const [sliderValue, setSliderValue] = useState<number>(500000); // Set initial value
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    setSliderValue(typeof newValue === "number" ? newValue : newValue[0]);
+    handleCapitalChange(typeof newValue === "number" ? newValue : 0);
+  };
+
   return (
     <div className={classNames(styles.root, className)}>
       <h1 className={styles.h1}>How much capital does your company need?</h1>
       <p className={styles.p}>
         Use the slider to select your company funding need
       </p>
-      <input
+      {/* <input
         className={styles.input}
         type="number"
         min={0}
@@ -30,7 +38,10 @@ export const Capital = ({
         step={10000}
         value={capitalAmount}
         onChange={handleCapitalChange}
-      />
+      /> */}
+      <div>
+        <InputSlider value={sliderValue} onChange={handleSliderChange} />
+      </div>
       {true && <Button buttonText="Next" onClick={handleForward} />}{" "}
       {/* Should be only if capital > 0 */}
       {/* Render button only if capital is selected */}
